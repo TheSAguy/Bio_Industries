b0068b81b944cb12df3e219b49687f5ed2b2b8e0
TheSAguy <vvwmisc@hotmail.com>
2018-01-16 14:11:35 -0800

    2.0.8 Release

--- Bio_Industries/control_tree.lua
+++ Bio_Industries/control_tree.lua
@@ -258,7 +258,6 @@ function is_value_as_index_in_table (value, tabl)
 end
 
 
-<<<<<<< HEAD
 terrains = require("libs/trees-and-terrains_alien_boimes")
 --[[
 
@@ -269,10 +268,6 @@ else
 end
 ]]
 
-=======
-terrains = require("libs/trees-and-terrains")
-
->>>>>>> 61c25610c28a6a7288cc85bd604fc1936d8ab149
 function summ_weight (tabl)
   local summ = 0
   for i, tree_weights in pairs (tabl) do
